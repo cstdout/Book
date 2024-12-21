361266edc549d2a8c96864f8b396a0372347dfae
cstdout <151808195+cstdout@users.noreply.github.com>
2024-12-21 10:10:37 +0400

    Update fictionbook.cpp

--- fictionbook.cpp
+++ fictionbook.cpp
@@ -18,9 +18,45 @@ FictionBook::FictionBook(FictionBookType type,
     _circulation = circulation;
 }
 
+FictionBook::FictionBook(const string& type,
+                         uint32_t soldCopies,
+                         const string& title,
+                         const string& authorFirstName,
+                         const string& authorLastName,
+                         uint32_t price,
+                         uint32_t pageCount,
+                         uint32_t circulation)
+{
+    setType(type);
+    _soldCopies = soldCopies;
+    _title = title;
+    _authorFirstName = authorFirstName;
+    _authorLastName = authorLastName;
+    _price = price;
+    _pageCount = pageCount;
+    _circulation = circulation;
+}
+
 void FictionBook::setType(FictionBookType type)
 {
     _type = type;
+}
+void FictionBook::setType(const string& type)
+{
+    FictionBookType fictionBookType;
+    if (type == "classic")
+    {
+        fictionBookType = FictionBookType::CLASSIC;
+    }
+    else if(type == "fantasy")
+    {
+        fictionBookType = FictionBookType::FANTASY;
+    }
+    else if(type == "science fiction")
+    {
+        fictionBookType = FictionBookType::SCIENCE_FICTION;
+    }
+    _type = fictionBookType;
 }
 void FictionBook::setSoldCopies(uint32_t soldCopies)
 {
@@ -31,39 +67,26 @@ ostream& operator<<(ostream& out, const FictionBook& b)
 {
     string fictionBookTypeStr;
     switch (b._type) {
-        case NOVEL:
-            fictionBookTypeStr = "novel";
+        case CLASSIC:
+            fictionBookTypeStr = "classic";
         break;
 
-        case STORY:
-            fictionBookTypeStr = "story";
+        case FANTASY:
+            fictionBookTypeStr = "fantasy";
         break;
 
-        case POEM:
-            fictionBookTypeStr = "poem";
+        case SCIENCE_FICTION:
+            fictionBookTypeStr = "science fiction";
         break;
     }
-    out << fictionBookTypeStr << ' ' << b._soldCopies << b._title << ' ' << b._authorFirstName << ' ' << b._authorLastName << ' ' << b._price << ' ' << b._pageCount << b._circulation;
+    out << fictionBookTypeStr << ' ' << b._soldCopies << ' ' << b._title << ' ' << b._authorFirstName << ' ' << b._authorLastName << ' ' << b._price << ' ' << b._pageCount << b._circulation;
     return out;
 }
 istream& operator>>(istream& in, FictionBook& b)
 {
     string fictionBookTypeStr;
     in >> fictionBookTypeStr;
-    FictionBookType fictionBookType;
-    if (fictionBookTypeStr == "novel")
-    {
-        fictionBookType = FictionBookType::NOVEL;
-    }
-    else if(fictionBookTypeStr == "story")
-    {
-        fictionBookType = FictionBookType::STORY;
-    }
-    else if(fictionBookTypeStr == "poem")
-    {
-        fictionBookType = FictionBookType::POEM;
-    }
-    b._type = fictionBookType;
+    b.setType(fictionBookTypeStr);
 
     in >> b._soldCopies >> b._title >> b._authorFirstName >> b._authorLastName >> b._price >> b._pageCount >> b._circulation;
     return in;
@@ -78,11 +101,11 @@ FictionBookType FictionBook::getType() const
 {
     return _type;
 }
-float FictionBook::popularity() const
+double FictionBook::popularity() const
 {
-    return (_soldCopies / _circulation) / (_soldCopies * 1000);
+    return (double(_soldCopies * _soldCopies) / double(1000 * _circulation));
 }
 float FictionBook::expensive() const
 {
-    return _price / _pageCount;
+    return float(_price) / float(_pageCount);
 }
